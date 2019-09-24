c4f85aefb729e21ca27f470961e04eba2676ec2d
Kayla <6lackmacaroon@gmail.com>
2019-09-24 10:03:03 -0600

    refactor database url config

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG') == 'True')
-DATABASE_URL = config('DATABASE_URL')
+
+DATABASE_URL = dj_database_url.config('DATABASE_URL', conn_max_age=600)
+
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS') #['*']
 
 
@@ -96,12 +98,12 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        dj_database_url.config(default=f'postgres://{DATABASE_URL}')
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # # 'HOST': os.environ.get('DB_HOST'),
-        # 'NAME': os.environ.get('DB_NAME'),
-        # 'USER': os.environ.get('DB_USER'),
-        # 'PASSWORD': os.environ.get('DB_PASS'),
+        # dj_database_url.config(default=f'postgres://{DATABASE_URL}')
+        'ENGINE': 'django.db.backends.postgresql',
+        # 'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
         
     }
 }
